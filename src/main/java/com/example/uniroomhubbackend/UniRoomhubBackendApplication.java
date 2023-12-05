85705792a1154a0d11de56f57516afe19c073f4b
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-12-05 15:35:48 +0700

    init project

---
+++ src/main/java/com/example/uniroomhubbackend/UniRoomhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniroomhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniRoomhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniRoomhubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uniroomhubbackend/UniRoomhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniroomhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniRoomhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
